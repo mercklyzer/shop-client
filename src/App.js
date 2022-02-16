7b84163e1694aea934e79f10cf2e2ff12365cef1
mercklyzer <mercklyzer@gmail.com>
2022-02-16 21:52:54 +0800

    Add slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+import 'swiper/css';
 
 function App() {
   return (
     <div className="App">
       <div className="flex justify-center items-center p-1.5 bg-teal-700 text-white text-sm">Super Deal! Free Shipping on Orders Over $50</div>
       <Navbar />
+      <Slider />
     </div>
   );
 }
