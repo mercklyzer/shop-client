2a5d66b78115c44d10edfcee471f3f4788b38076
mercklyzer <mercklyzer@gmail.com>
2022-02-25 01:43:31 +0800

    Fix bug on product price on cart

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -9,12 +9,12 @@ const cartSlice = createSlice({
     },
     reducers: {
         addProduct: (state, action) => {
-            console.log(action);
             let product = {...action.payload.product, quantity: action.payload.quantity, total: action.payload.total}
 
             let i = state.products.findIndex(prod => prod._id === product._id && prod.color === product.color && prod.size === product.size)
             if(i >= 0){
                 state.products[i].quantity += product.quantity
+                state.products[i].total += product.total
             }
             else{
                 state.products.push(product)
