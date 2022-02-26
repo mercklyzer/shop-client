9bcf924b1bf309114ccf98c485adbb67bbf71c24
mercklyzer <mercklyzer@gmail.com>
2022-02-26 17:21:03 +0800

    Add api calls file for login

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import { loginFailure, loginStart, loginSuccess } from "./userRedux"
+
+export const login = async (dispatch, user) => {
+    dispatch(loginStart())
+    try{
+        const res = await axios(
+            "http://localhost:5000/auth/login",
+            user
+        )
+        dispatch(loginSuccess(res.data))
+    }
+    catch(err){
+        dispatch(loginFailure())
+    }
+}
