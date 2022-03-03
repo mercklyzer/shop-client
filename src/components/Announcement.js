b35a7f8e69a3a50021b39ce89cfbd2d0d5590229
mercklyzer <mercklyzer@gmail.com>
2022-03-03 21:06:08 +0800

    Modify announcer component

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Announcement = props => {
     return (
-        <div className="flex justify-center items-center p-1.5 bg-teal-700 text-white text-sm">Super Deal! Free Shipping on Orders Over $50</div>
+        <div className="flex justify-center items-center p-2 bg-zinc-800 font-semibold text-white text-sm">24 HOUR FLASH SALE - MEMBERS SAVE 20% + 20% VOUCHER</div>
     )
 }
 
