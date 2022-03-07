8ec8353ec86a6df4ba99dadedd3c56352ec8206c
mercklyzer <mercklyzer@gmail.com>
2022-03-07 22:44:28 +0800

    Unfinish products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,66 +2,51 @@ import axios from "axios";
 import React, { useEffect, useState } from "react"
 import Product from "./Product"
 
-const Products = ({category, filters, order}) => {
+const Products = ({category, filters, order, data}) => {
     // fetch the data here
 
-    const [isLoading, setIsLoading] = useState(true)
-    const [products, setProducts] = useState()
-    const [filteredProducts, setFilteredProducts] = useState()
+    // const [isLoading, setIsLoading] = useState(true)
+    // const [products, setProducts] = useState()
+    // const [filteredProducts, setFilteredProducts] = useState()
 
-    useEffect(() => {
-        const getProducts = async () => {
-            setIsLoading(true)
-            try{
-                const res = await axios.get(
-                    category? 
-                        `http://localhost:5000/products?category=${category}`
-                        : "http://localhost:5000/products"
-                )
-                console.log(res);
-                setProducts(res.data)
-            }
-            catch(err){
-                console.log(err);
-            }
-            setIsLoading(false)            
-        }
+    // useEffect(() => {
+    //     const getProducts = async () => {
+    //         setIsLoading(true)
+    //         try{
+    //             const res = await axios.get(
+    //                 category? 
+    //                     `http://localhost:5000/products?category=${category}`
+    //                     : "http://localhost:5000/products"
+    //             )
+    //             console.log(res);
+    //             setProducts(res.data)
+    //         }
+    //         catch(err){
+    //             console.log(err);
+    //         }
+    //         setIsLoading(false)            
+    //     }
 
-        getProducts()
-    }, [category])
+    //     getProducts()
+    // }, [category])
 
-    useEffect(() => {
-        if(category && !isLoading){
-            setFilteredProducts(
-                products?.filter(item => Object.entries(filters)
-                .every(([key, value]) => item[key].includes(value)))
-                )
-            console.log(filteredProducts);
-        }
-    }, [products, category, filters, isLoading])
-
-    useEffect(() => {
-        if(order === 'newest'){
-            setFilteredProducts((prev) => [...prev].sort((a, b) => a.createdAt - b.createdAt))
-        }
-        else if(order === 'asc'){
-            setFilteredProducts((prev) => [...prev].sort((a, b) => a.pricec - b.price))
-        }
-        else if(order === 'asc'){
-            setFilteredProducts((prev) => [...prev].sort((a, b) => b.price - a.price))
-        }
-    }, [order])
-
+    // useEffect(() => {
+    //     if(category && !isLoading){
+    //         setFilteredProducts(
+    //             products?.filter(item => Object.entries(filters)
+    //             .every(([key, value]) => item[key].includes(value)))
+    //             )
+    //         console.log(filteredProducts);
+    //     }
+    // }, [products, category, filters, isLoading])
 
 
     return (
-        <div className="grid grid-rows-[8] gap-2 mt-2 sm:grid-rows-4 sm:grid-cols-2 md:grid-rows-3 md:grid-cols-3">
+        <div className="grid grid-cols-4 gap-10">
             {
-                !isLoading?
-                    (category?
-                        filteredProducts?.map(({_id, img}) => <Product key={_id} id={_id} image={img}/>)
-                        : products?.slice(0, 8).map(({_id, img}) => <Product key={_id} id={_id} image={img} />))
-                : <div>Fetching data...</div>
+
+                data.map(({id, img}) => <Product key={id} id={id} image={img}/>)
+
             }
         </div>
     )
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -9,6 +9,11 @@ const data = productsData
 const ProductList = props => {
     const location = useLocation()
     const category = location.pathname.split("/")[3]
+
+    const capitalize = (s) => {
+        return s[0].toUpperCase() + s.slice(1)
+    }
+
     // const [filters, setFilters] = useState({
     //     color: 'White',
     //     size: 'XS'
@@ -33,11 +38,11 @@ const ProductList = props => {
             <div className="h-[calc(80vh-6rem)] w-full bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url(${sofasHeader})`}}>
                 <div className="h-full w-full opacity-30 bg-black"></div>    
             </div>
-            
-            <div className="section">
-                <div className="text-left font-bold text-4xl mt-8 mb-8">{category}</div>
 
-                {/* <Products category={category} filters={filters} order={sort} data={data} /> */}
+            <div className="section">
+                <div className="text-left font-semibold text-zinc-700 text-3xl mt-8 mb-8 tracking-wider">{capitalize(category)}</div>
+
+                <Products category={category} data={data} />
             </div>
         </>
     )
