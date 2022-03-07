da9252fde7a8c7806dbccda9022985522af6ebfa
mercklyzer <mercklyzer@gmail.com>
2022-03-07 22:28:23 +0800

    Add category header picture (sofas)

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,13 +2,14 @@ import React from "react"
 import { useNavigate } from "react-router-dom"
 
 const Category = props => {
+    const category = props.title.toLowerCase()
     let navigate = useNavigate()
 
     return (
         <div className={`bg-bottom bg-cover h-72 sm:h-96 md:h-[40rem] w-full flex justify-center items-center flex-col relative ${props.feature? 'sm:row-span-2 sm:h-full md:h-full': ''}`} style={{backgroundImage: `url(${props.image}`}}>
             <div className="overlay"></div>
             <div className="text-white text-3xl font-bold mb-4 z-10">{props.title}</div>
-            <div className="bg-white text-stone-600 py-1 px-3 z-10 cursor-pointer" onClick={() => navigate('/shop-client/products')}>SHOP NOW</div>
+            <div className="bg-white text-stone-600 py-1 px-3 z-10 cursor-pointer" onClick={() => navigate(`/shop-client/products/${category}`)}>SHOP NOW</div>
         </div>
     )
 }
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -2,66 +2,44 @@ import React, { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import Products from "../components/Products"
 import { productsData } from "../data/productsData"
+import sofasHeader from '../assets/sofas.jpeg'
 
 const data = productsData
 
 const ProductList = props => {
     const location = useLocation()
     const category = location.pathname.split("/")[3]
-    const [filters, setFilters] = useState({
-        color: 'White',
-        size: 'XS'
-    })
-    const [sort, setSort] = useState('newest')
+    // const [filters, setFilters] = useState({
+    //     color: 'White',
+    //     size: 'XS'
+    // })
+    // const [sort, setSort] = useState('newest')
 
-    const handleFilters = (e) => {
-        setFilters((filters) => {
-            return {
-                ...filters,
-                [e.target.name]: e.target.value
-            }
-        })
-    }
+    // const handleFilters = (e) => {
+    //     setFilters((filters) => {
+    //         return {
+    //             ...filters,
+    //             [e.target.name]: e.target.value
+    //         }
+    //     })
+    // }
 
-    useEffect(() => {
-        console.log(filters);
-    }, [filters])
+    // useEffect(() => {
+    //     console.log(filters);
+    // }, [filters])
 
     return (
-        <div className="section">
-            <div className="text-left font-bold text-4xl mt-8 mb-8">{category}</div>
-            <div className="flex flex-col items-start mb-6 md:flex-row md:justify-between md:items-start">
-                <div className="mb-4 flex items-center md:mb-0"> 
-                    <div className="text-lg font-semibold ">Filter Products</div>
-                    <select name="color" className="mx-4 px-4 py-1 border border-black" onChange={handleFilters}>
-                        <option value="" disabled>Color</option>
-                        <option value="White">White</option>
-                        <option value="Black">Black</option>
-                        <option value="Red">Red</option>
-                        <option value="Blue">Blue</option>
-                        <option value="Yellow">Yellow</option>
-                        <option value="Green">Green</option>
-                    </select>
-                    <select name="size" className="px-4 py-1 border border-black" onChange={handleFilters}>
-                        <option value="" disabled>Size</option>
-                        <option value="XS">XS</option>
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                    </select>
-                </div>
-                <div className="flex items-center">
-                    <div className="text-lg font-semibold">Sort Products</div>
-                    <select className="ml-4 px-4 py-1 border border-black" onChange={(e) => setSort(e.target.value)}>
-                        <option value="newest" selected>Newest</option>
-                        <option value="asc">Price (ASC)</option>
-                        <option value="desc">Price (DESC)</option>
-                    </select>
-                </div>
+        <>
+            <div className="h-[calc(80vh-6rem)] w-full bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url(${sofasHeader})`}}>
+                <div className="h-full w-full opacity-30 bg-black"></div>    
             </div>
-            <Products category={category} filters={filters} order={sort} data={data} />
-        </div>
+            
+            <div className="section">
+                <div className="text-left font-bold text-4xl mt-8 mb-8">{category}</div>
+
+                {/* <Products category={category} filters={filters} order={sort} data={data} /> */}
+            </div>
+        </>
     )
 }
 
