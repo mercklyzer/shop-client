508b6638da3be60efa7bbad90e4fb9d8f532ec99
mercklyzer <mercklyzer@gmail.com>
2022-03-07 23:38:07 +0800

    Adjust spacing

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -21,18 +21,18 @@ const ProductList = props => {
                 <div className="h-full w-full opacity-30 bg-black"></div>    
             </div>
 
-            <div className="section py-8">
+            <div className="section">
                 <div className="text-left font-semibold text-zinc-700 text-3xl mt-8 mb-8 tracking-wider">{capitalize(category)}</div>
             </div>
 
-            <div className="bg-zinc-100">
+            <div className="bg-zinc-100 mb-8">
                 <div className="section flex justify-between items-center">
                     <select className={`bg-zinc-100 outline-none pr-12 py-2 focus:bg-zinc-100 font-semibold text-zinc-700`}>
-                        <option className="text-zinc-500 font-semibold">Pillows</option>
-                        <option className="text-zinc-500 font-semibold">Beds</option>
-                        <option className="text-zinc-500 font-semibold">Sofas</option>
-                        <option className="text-zinc-500 font-semibold">Chairs</option>
-                        <option className="text-zinc-500 font-semibold">Tables</option>
+                        <option className="text-zinc-500 font-semibold" selected={category === 'pillows'}>Pillows</option>
+                        <option className="text-zinc-500 font-semibold" selected={category === 'beds'}>Beds</option>
+                        <option className="text-zinc-500 font-semibold" selected={category === 'sofas'}>Sofas</option>
+                        <option className="text-zinc-500 font-semibold" selected={category === 'chairs'}>Chairs</option>
+                        <option className="text-zinc-500 font-semibold" selected={category === 'tables'}>Tables</option>
                     </select>
                     <select className={`bg-zinc-100 outline-none pr-12 py-2 focus:bg-zinc-100 font-semibold text-zinc-700`}>
                         <option className="text-zinc-500 font-semibold">Popular</option>
@@ -42,7 +42,7 @@ const ProductList = props => {
                 </div>
             </div>
 
-            <div className="section">
+            <div className="section py-8">
                 <Products category={category} data={data} />
             </div>
         </>
