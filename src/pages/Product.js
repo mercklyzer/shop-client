b038f27115834f4cbd8bed52110da961913ee566
mercklyzer <mercklyzer@gmail.com>
2022-03-08 21:52:30 +0800

    Add slider for product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -6,87 +6,50 @@ import Color from "../components/Color"
 import Quantity from "../components/Quantity"
 import { addProduct } from "../redux/cartRedux"
 
+import product1 from '../assets/product_1.jpg'
+import product2 from '../assets/product_2.jpg'
+
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper";
+
 const Product = props => {
     const location = useLocation()
     const id = location.pathname.split("/")[3]
-    const [isLoading, setIsLoading] = useState(true)
-    const [product, setProduct] = useState()
-    const [selectedSize, setSelectedSize] = useState()
-    const [selectedColor, setSelectedColor] = useState()
-    const [quantity, setQuantity] = useState(0)
 
-    const setQuantityHandler = (operation) => {
-        setQuantity(qty => {
-            let newQty = qty
-            if(operation === '-' && qty > 0){
-                newQty = qty - 1
-            }
-            else if(operation === '+'){
-                newQty = qty + 1
-            }
-            return newQty
-        })
-    }
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        const getProduct = async () => {
-            setIsLoading(true)
-            try{
-                const res = await axios.get(`http://localhost:5000/products/find/${id}`)
-                setProduct(res.data)
-            }
-            catch(err){
-                console.log(err);
-            }
-            setIsLoading(false)
-        }
-
-        getProduct()
-    }, [id])
-
-    useEffect(() => {
-        if(product){
-            setSelectedSize(product.size[0])
-            setSelectedColor(product.color[0])
-        }
-    }, [product])
-
-
-    const handleClick = () => {
-        dispatch(addProduct({product: {...product, size: selectedSize, color: selectedColor}, quantity, total:product.price*quantity}))
-    }
 
     return (
-        !isLoading? <div className="section grid grid-cols-1 md:grid-rows-1 md:grid-cols-2 md:gap-x-8 md:mt-8">
-            <img className="mb-4 object-contain w-full" src={product.img} />
-            <div>
-                <div className="text-4xl text-left mb-8 lg:text-5xl">{product.title}</div>
-                <div className="text-left mb-4">{product.desc}</div>
-                <div className="text-left text-2xl mb-4 md:text-3xl lg:text-4xl">$ {product.price}</div>
-                <div className="md:flex md:justify-between 2xl:max-w-[50%]">
-                    <div className="flex items-center mb-4">
-                        <span className="text-xl font-light">Color</span>
-                        {product.color.map((color, i) => i === 0? <Color key={i} color={color} className={`ml-4 cursor-pointer`} selected={selectedColor === color} setSelectedColor={setSelectedColor}/> : <Color key={i} color={color} className={`ml-2 cursor-pointer`} selected={selectedColor === color} setSelectedColor={setSelectedColor}/>)}
-                    </div>
-                    <div className="flex items-center mb-4">
-                        <span className="text-xl font-light mr-7">Size</span>
-                        <select className="border border-black px-2 py-1 select-none" onChange={(e) => setSelectedSize(e.target.value)}>
-                            {product.size.map((size, i) => i === 0? 
-                                <option key={i} value={size}>{size}</option>
-                                :<option key={i} value={size}>{size}</option>)
-                            }
-                        </select>
-                    </div>
-                </div>
-                <div className="flex flex-col justify-center items-start md:flex-row md:items-center md:justify-between 2xl:max-w-[50%]">
-                    <Quantity qty={quantity} setQty={setQuantityHandler} className="mb-8"/>
-                    <button onClick={handleClick} disabled={quantity <= 0} className="py-2 px-4 mb-8 border-2 border-emerald-600 cursor-pointer hover:bg-emerald-600 hover:text-white duration-200 disabled:opacity-50 disabled:cursor-auto disabled:transform-none disabled:transition-none disabled:bg-emerald-600 disabled:text-white select-none">ADD TO CART</button>
-                </div>
+        <div className="section grid grid-cols-12 gap-12 w-full">
+            <div className="col-span-7">
+            <Swiper
+                pagination={{
+                type: "fraction",
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img className="h-auto w-full object-cover" src={product1} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="h-auto w-full object-cover" src={product2} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="h-auto w-full object-cover" src={product1} />
+                </SwiperSlide>
+            </Swiper>
+            </div>
+
+            <div className="col-span-5 text-left bg-red-700">
+                Angelo Dining Chair
             </div>
         </div>
-        : <div>Fetching data...</div>
     )
 }
 
