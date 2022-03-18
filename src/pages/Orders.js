38436c2c5945c18cb7b762de8370bc076883081a
mercklyzer <mercklyzer@gmail.com>
2022-03-18 23:25:14 +0800

    Add responsive orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -10,108 +10,45 @@ const Orders = props => {
         <div className="section w-full">
             <div className="text-3xl font-light my-4">YOUR ORDERS</div>
 
-            <div className="flex border rounded-lg mb-2 flex-col p-8">
+            <div className="flex border rounded-lg mb-2 flex-col p-4 md:p-8">
                 <div className="text-left w-full">
                     <div className="float-right text-emerald-700 px-2 py-1 rounded-lg bg-green-300">Completed</div>
                     <div className="font-bold text-orange-500">12/12/2022</div>
                     <div className="font-bold text-lg text-zinc-700">Order ID</div>
-                    <div className="ml-12 text-zinc-600 font-semibold">Ggh1231sdasd23123zzxcca2231</div>
+                    <div className="ml-4 text-zinc-600 font-semibold text-sm">Ggh1231sdasd23123zzxcca2231</div>
                     <div className="font-bold text-lg text-zinc-700">Deliver To</div>
-                    <div className="ml-12 text-zinc-600 font-semibold">B14 L5 HAMMER ST., KENSINGTON 22, LANCASTER NEW CITY, GEN. TRIAS, CAVITE</div>
+                    <div className="ml-4 text-zinc-600 font-semibold text-sm">B14 L5 HAMMER ST., KENSINGTON 22, LANCASTER NEW CITY, GEN. TRIAS, CAVITE</div>
                     <div className="font-bold text-lg text-zinc-700">Total</div>
-                    <div className="ml-12 text-zinc-600 font-semibold">$6000</div>
+                    <div className="ml-4 text-zinc-600 font-semibold text-sm">$6000</div>
                     <div className="font-bold text-lg text-zinc-700">Items</div>
                 </div>
 
-                <div className="py-4 flex">
+                <div className="pt-4 flex flex-col sm:flex-row">
                     <img
-                        className="w-full h-auto max-w-[290px] object-contain px-4" 
+                        className="w-full h-auto max-w-[290px] object-contain px-4 m-auto sm:m-0" 
                         src={cart.products[0].displayImg} 
                     />
-                    <div className="w-full flex justify-center items-center">
+                    <div className="w-full flex flex-col justify-center items-center mt-4 md:flex-row">
                         <div className="w-1/3 font-bold text-xl text-zinc-700">Ejy Bed</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">3 pcs</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">$6000</div>
+                        <div className="w-1/3 font-semibold text-lg text-zinc-700">3 pcs</div>
+                        <div className="w-1/3 font-semibold text-lg text-zinc-700">$6000</div>
                     </div>
                 </div>
-
-                <div className="py-4 flex">
+                <div className="pt-4 flex flex-col sm:flex-row">
                     <img
-                        className="w-full h-auto max-w-[290px] object-contain px-4" 
+                        className="w-full h-auto max-w-[290px] object-contain px-4 m-auto sm:m-0" 
                         src={cart.products[0].displayImg} 
                     />
-                    <div className="w-full flex justify-center items-center">
+                    <div className="w-full flex flex-col justify-center items-center mt-4 md:flex-row">
                         <div className="w-1/3 font-bold text-xl text-zinc-700">Ejy Bed</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">3 pcs</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">$6000</div>
+                        <div className="w-1/3 font-semibold text-lg text-zinc-700">3 pcs</div>
+                        <div className="w-1/3 font-semibold text-lg text-zinc-700">$6000</div>
                     </div>
                 </div>
-
-
-
-                {/* <div className="w-full">      */}
-                    
-
-
-                
-                {/* {
-                    cart.products.map(({_id, title, desc, displayImg, price, quantity, total}, i) => (
-                        <CartProduct key={i} id={_id} title={title} description={desc} image={displayImg} price={price} quantity={quantity} total={total}/>
-                    ))
-                }                */}
-                {/* </div> */}
             </div>
 
-            <div className="flex border rounded-lg mb-2 flex-col p-8">
-                <div className="text-left w-full">
-                    <div className="float-right text-emerald-700 px-2 py-1 rounded-lg bg-green-300">Completed</div>
-                    <div className="font-bold text-lg text-zinc-700">Order ID</div>
-                    <div className="ml-12">Ggh1231sdasd23123zzxcca2231</div>
-                    <div className="font-bold text-lg text-zinc-700">Deliver To</div>
-                    <div className="ml-12">B14 L5 HAMMER ST., KENSINGTON 22, LANCASTER NEW CITY, GEN. TRIAS, CAVITE</div>
-                    <div className="font-bold text-lg text-zinc-700">Total</div>
-                    <div className="ml-12">$6000</div>
-                    <div className="font-bold text-lg text-zinc-700">Items</div>
-                </div>
-
-                <div className="py-4 flex">
-                    <img
-                        className="w-full h-auto max-w-[290px] object-contain px-4" 
-                        src={cart.products[0].displayImg} 
-                    />
-                    <div className="w-full flex justify-center items-center">
-                        <div className="w-1/3 font-bold text-xl text-zinc-700">Ejy Bed</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">3 pcs</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">$6000</div>
-                    </div>
-                </div>
-
-                <div className="py-4 flex">
-                    <img
-                        className="w-full h-auto max-w-[290px] object-contain px-4" 
-                        src={cart.products[0].displayImg} 
-                    />
-                    <div className="w-full flex justify-center items-center">
-                        <div className="w-1/3 font-bold text-xl text-zinc-700">Ejy Bed</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">3 pcs</div>
-                        <div className="w-1/3 font-semibold text-xl text-zinc-700">$6000</div>
-                    </div>
-                </div>
 
 
-
-                {/* <div className="w-full">      */}
-                    
-
-
-                
-                {/* {
-                    cart.products.map(({_id, title, desc, displayImg, price, quantity, total}, i) => (
-                        <CartProduct key={i} id={_id} title={title} description={desc} image={displayImg} price={price} quantity={quantity} total={total}/>
-                    ))
-                }                */}
-                {/* </div> */}
-            </div>
         </div>
     )
 }
