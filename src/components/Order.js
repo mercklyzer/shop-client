f71c6406d23486dc826b0e38b25256746b558527
mercklyzer <mercklyzer@gmail.com>
2022-03-19 16:47:06 +0800

    Use momentjs

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,13 @@
 import React from "react";
 import OrderProduct from "./OrderProduct";
+import moment from 'moment'
 
 const Order = ({data}) => {
     return (
         <div className="flex border rounded-lg mb-2 flex-col p-4 md:p-8">
             <div className="text-left w-full">
                 <div className={`float-right ${data.status === 'Pending'? 'text-orange-500 bg-yellow-200':'text-emerald-700 bg-green-300'}  px-2 py-1 rounded-lg`}>{data.status}</div>
-                <div className='font-bold text-orange-500'>{data.updatedAt}</div>
+                <div className='font-bold text-orange-500'>{moment(data.updatedAt).format('LL')}</div>
                 <div className="font-bold text-lg text-zinc-700">Order ID</div>
                 <div className="ml-4 text-zinc-600 font-semibold text-sm">{data._id}</div>
                 <div className="font-bold text-lg text-zinc-700">Deliver To</div>
