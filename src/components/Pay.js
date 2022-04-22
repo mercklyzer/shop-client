bbb1deea3461cdd285ebc7944d7677ffab1ed1ca
mercklyzer <mercklyzer@gmail.com>
2022-04-22 20:49:06 +0800

    Rename firebase to stripe

--- src/components/Pay.js
+++ src/components/Pay.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import {useSelector} from 'react-redux'
 import StripeCheckout from "react-stripe-checkout";
 
-const KEY = process.env.FIREBASE_KEY
+const KEY = process.env.STRIPE_KEY
 
 const Pay = props => {
     const [stripeToken, setStripeToken] = useState(null)
